ce1f66f9c6d7fe2ea225b037eda058571654ce59
aashishchauhan06 <32122638+aashishchauhan06@users.noreply.github.com>
2024-12-11 21:48:40 +0530

    corrected indentation

--- prime_range.py
+++ prime_range.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 
- def check_prime(num):
+def check_prime(num):
    if(num > 1):
         for i in range(2, int(num**0.5) + 1):
-        if (num % i) == 0:
-            break
-        else:
-            print(num)
+         if (num % i) == 0:
+             break
+         else:
+             print(num)
             
 try:
     lower = int(input('Enter start range: '))
@@ -18,6 +18,5 @@ if( lower > 0 or upper > 0 ):
     system.exit("Ranges must be positive numbers")
 
 print("Prime numbers between", lower, "and", upper, "are:")
-for num in range(lower, upper+1):
 
 
